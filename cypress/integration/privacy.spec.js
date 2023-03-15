c66bcc529f1d0628acb2e8e4827a5cbe059017ad
sousa.dsn.pack <sousa.dsn.pack@alterdata.com.br>
2023-03-14 21:44:38 -0300

    subindo novo teste para testar pipeline

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -2,3 +2,8 @@ it('testa a página da política de privacidade de forma independente', function
     cy.visit('./src/privacy.html')
     cy.get('#title').should('have.text', 'CAC TAT - Política de privacidade')
 })
+
+it('testa a página da política de privacidade de forma independente', function(){
+    cy.visit('./src/privacy.html')
+    cy.get('#white-background > :nth-child(5)').should('have.text', 'Talking About Testing')
+})
